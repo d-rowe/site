d4078cc76de67884094a1e2233662b03ddd5eef5
Daniel Rowe <danielrowetech@gmail.com>
2024-11-23 16:07:41 -0800

    init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+          <h1>d-rowe</h1>
+          <p>building things. more to come soon.</p>
+      </main>
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  );
+}
