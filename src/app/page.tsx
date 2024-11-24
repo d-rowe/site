d61a090d57174e4c10f7b3c4b42fb2ce7dcb5574
Daniel Rowe <danielrowetech@gmail.com>
2024-11-23 18:03:23 -0800

    update styles, content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
+import Link from 'next/link';
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <header className={styles.header}>
+        <Link href='/posts'>posts</Link>
+      </header>
       <main className={styles.main}>
+        <div className={styles.intro}>
           <h1>d-rowe</h1>
-          <p>building things. more to come soon.</p>
+          <p>field notes on making</p>
+        </div>
       </main>
       <footer className={styles.footer}>
+        <p>made with ❤️ by daniel rowe</p>
       </footer>
     </div>
   );
